8d301e1f3c56648c1dec6b2882dbc581a6dca9bc
Mateus Koppe <mateus.koppe@gmail.com>
2020-11-12 21:04:19 -0300

    Implement print list function

--- src/linked-list.asm
+++ src/linked-list.asm
@@ -1,9 +1,9 @@
 	.globl main
 	
 	.data
-header:			.string "\tGerenciador de listas 9000\n"
+header:			.string "\tGerenciator de listas 9000\n"
 insertline:		.string "\t 1) Insere um elemento na lista\n"
-removevalline:		.string "\t 2) Remove por valor na lista\n"
+#removevalline:		.string "\t 2) Remove por valor na lista\n"
 removeindline:		.string "\t 3) Remove por índice na lista\n"
 printline:		.string "\t 4) Imprime lista\n"
 exitline:		.string "\t 0) Sai do programa\n"
@@ -11,54 +11,62 @@ exitline:		.string "\t 0) Sai do programa\n"
 	.text
 main:
 	nop
-	addi s1, sp,   0  # pointer to stack
-	addi s2, zero, 0  # pointer to last element
-	addi s3, zero, 0  # size of the list
-	addi s4, zero, 0  # size of removed elements
-	addi s5, zero, 0  # size of elements added
+	addi s0, sp,   0  # pointer to start of the stack
 	
-menu:                     # prints the menu
+menu:
+# prints the menu
 	la a0, header     # loads the string to a0
 	jal printstr      # prints it
 	la a0, insertline # repeat until line 30
 	jal printstr
-	la a0, removevalline
-	jal printstr
-	la a0, printline
+	la a0, printline # repeat until line 30
 	jal printstr
 	la a0, exitline
-	jal printstr      
-
-readint:                  # label to read input from the user to a0
-	li a7, 5
-	ecall
+	jal printstr
 	
-optionmenu:               # process the user input 
-	addi t0, zero, 0
+# process the user input 
+	jal readint
+	li t0, 0
 	beq  a0, t0, exit 
-	addi t0, zero, 1
+	li t0, 1
 	beq  a0, t0, insertval
-	addi t0, zero, 2
-	beq  a0, t0, removeval
-	addi t0, zero, 3
-	beq  a0, t0, removeind
-	addi t0, zero, 4
-	beq  a0, t0, printlist
+	li t0, 4
+	beq  a0, t0, print_list
+	li t0, 2
 	j menu
 
 insertval:
-
-removeval:
-
-removeind:
-
-printlist:
+	jal readint
+	add t0, zero, a0
+	sw t0, 0(sp)
+	addi sp, sp, -4
+	j menu
 	
+###
+# Print List
+###
+print_list:
+	add t0, zero, s0
+print_list_loop:
+	beq t0, sp, menu
+	lw a0, 0(t0) 
+	jal print_int
+	addi t0, t0, -4
+	j print_list_loop
+
+### Helpers
+readint:                  # label to read input from the user to a0
+	li a7, 5
+	ecall
+	ret
+
 printstr:                 # auxiliary function to print string loaded to a0
 	li a7, 4
 	ecall
 	ret
-	
+print_int:
+	li a7, 1
+	ecall
+	ret
 
-	
 exit:                     # exits
